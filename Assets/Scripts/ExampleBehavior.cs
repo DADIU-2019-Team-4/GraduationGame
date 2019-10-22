d3e80a2576174a56fe312e3a6422bc0864aa75d9
Vladislav Zenkevic <vladislav.zenkevic@gmail.com>
2019-10-22 10:11:13 +0200

    Scripts and scenes from Optional Research ported to this project

---
+++ Assets/Scripts/ExampleBehavior.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleBehavior : MonoBehaviour
+{
+    [DraggablePoint] public Vector3 SpawnPosition;
+    [DraggablePoint] public Vector3 SpawnPosition2;
+    [DraggablePoint] public Vector3 SpawnPosition3;
+    public GameObject SpawnableObject;
+
+    public void Spawn()
+    {
+        Instantiate(SpawnableObject, SpawnPosition, Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/LoadResources.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class LoadResources : MonoBehaviour
+{
+    private AssetBundle bundleRequest;
+    private UnityWebRequest request;
+    public string link;
+
+    private void Start()
+    {
+        StartCoroutine(LoadAsset());
+    }
+
+    private void Update()
+    {
+
+    }
+    IEnumerator  LoadAsset()
+    {
+        Time.timeScale = 0;
+        request = UnityWebRequestAssetBundle.GetAssetBundle(link);
+        yield return request.SendWebRequest();
+        bundleRequest = DownloadHandlerAssetBundle.GetContent(request);
+        if (bundleRequest != null)
+        {
+            GameObject platform = bundleRequest.LoadAsset<GameObject>("CHIMERA_model");
+            Instantiate(platform);
+        }
+        else
+            Debug.Log("Asset bundle wasn't received");
+        Time.timeScale = 1;
+    }
+}
