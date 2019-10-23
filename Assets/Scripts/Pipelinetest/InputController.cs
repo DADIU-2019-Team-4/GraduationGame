603af0a5d094345fb017f56f1cb2cff419dc5b87
magei00 <magnuseilersen@yahoo.dk>
2019-10-23 15:06:13 +0200

    Added phone movement controls.

---
+++ Assets/Scripts/Pipelinetest/InputController.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    [SerializeField]
+    private float minSwipeDistanceInPercentage = 0.10f;
+    private float verticalSwipeDistance;
+    private float horizontalSwipeDistance;
+
+    private Vector3 firstPosition;
+    private Vector3 lastPosition;
+    private bool hasSwiped;
+
+    private bool trackMouse;
+
+    private PlayerMovement playerMovement;
+    private float timer;
+
+    [SerializeField]
+    private bool canSwipeDiagonal;
+
+    [SerializeField]
+    private float coyoteTime = 0.5f;
+
+    private float coyoteTimer;
+
+    private bool swipeRegistered;
+
+    private void Awake()
+    {
+        playerMovement = FindObjectOfType<PlayerMovement>();
+    }
+
+    private void Start()
+    {
+        verticalSwipeDistance = Screen.height * minSwipeDistanceInPercentage;
+        horizontalSwipeDistance = Screen.width * minSwipeDistanceInPercentage;
+    }
+
+
+    /// <summary>
+    /// Update function.
+    /// </summary>
+    public void Update()
+    {
+        HandleInput();
+
+        if (swipeRegistered)
+        {
+            if (!hasSwiped && coyoteTimer < coyoteTime)
+            {
+                CheckSwipe();
+            }
+            // todo fix this
+        }
+    }
+
+
+    /// <summary>
+    /// Handles the input.
+    /// </summary>
+    private void HandleInput()
+    {
+#if UNITY_EDITOR || UNITY_STANDALONE
+        PcInput();
+#elif UNITY_ANDROID || UNITY_IOS
+        MobileInput();
+#endif
+    }
+
+
+    /// <summary>
+    /// Handles mobile input.
+    /// </summary>
+    private void MobileInput()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            if (touch.phase == TouchPhase.Began)
+            {
+                firstPosition = touch.position;
+                lastPosition = touch.position;
+            }
+            else if (touch.phase == TouchPhase.Stationary)
+            {
+                timer += Time.deltaTime;
+                if (timer >= playerMovement.ChargeThreshold)
+                {
+                    playerMovement.ChargeDash();
+                    playerMovement.IsDashCharged = true;
+                }
+            }
+            else if (touch.phase == TouchPhase.Moved)
+            {
+                if (!hasSwiped)
+                {
+                    lastPosition = touch.position;
+                    CheckSwipe();
+                }
+                else if (!swipeRegistered)
+                {
+                    swipeRegistered = true;
+                }
+            }
+            else if (touch.phase == TouchPhase.Ended)
+            {
+                hasSwiped = false;
+                timer = 0;
+                playerMovement.ResetDash();
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Handles pc input.
+    /// </summary>
+    private void PcInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            trackMouse = true;
+            firstPosition = Input.mousePosition;
+            lastPosition = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            timer += Time.deltaTime;
+            if (timer >= playerMovement.ChargeThreshold)
+            {
+                playerMovement.ChargeDash();
+                playerMovement.IsDashCharged = true;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            trackMouse = false;
+            timer = 0;
+            playerMovement.ResetDash();
+        }       
+
+        if (trackMouse)
+        {
+            lastPosition = Input.mousePosition;
+            CheckSwipe();
+        }
+    }
+
+
+    /// <summary>
+    /// Checks how to player has swiped.
+    /// </summary>
+    private void CheckSwipe()
+    {
+        Vector3 directionVector = lastPosition - firstPosition;
+
+        //if (SwipedLongEnough(directionVector)) return;
+
+        if (Math.Abs(directionVector.x) > horizontalSwipeDistance)
+        {
+            if (playerMovement.IsDashCharged)
+                playerMovement.StartDash(HorizontalSwipe());
+            else
+                playerMovement.StartMove(HorizontalSwipe());
+
+            hasSwiped = true;
+        }
+        else if (Math.Abs(directionVector.y) > verticalSwipeDistance)
+        {
+            if (playerMovement.IsDashCharged)
+                playerMovement.StartDash(VerticalSwipe());
+            else
+                playerMovement.StartMove(VerticalSwipe());
+
+            hasSwiped = true;
+        }
+        else
+        {
+            if (canSwipeDiagonal && Math.Abs(directionVector.y) > verticalSwipeDistance / 3 &&
+                Math.Abs(directionVector.x) > horizontalSwipeDistance / 3)
+            {
+                if (lastPosition.x > firstPosition.x)
+                {
+                    if (lastPosition.y > firstPosition.y)
+                    {
+                        if (playerMovement.IsDashCharged)
+                            playerMovement.StartDash(new Vector3(1, 0, 1));
+                        else
+                            playerMovement.StartMove(new Vector3(1, 0, 1));
+
+                        hasSwiped = true;
+                    }
+                    else
+                    {
+                        if (playerMovement.IsDashCharged)
+                            playerMovement.StartDash(new Vector3(1, 0, -1));
+                        else
+                            playerMovement.StartMove(new Vector3(1, 0, -1));
+
+                        hasSwiped = true;
+                    }
+                }
+                else
+                {
+                    if (lastPosition.y > firstPosition.y)
+                    {
+                        if (playerMovement.IsDashCharged)
+                            playerMovement.StartDash(new Vector3(-1, 0, 1));
+                        else
+                            playerMovement.StartMove(new Vector3(-1, 0, 1));
+
+                        hasSwiped = true;
+                    }
+                    else
+                    {
+                        if (playerMovement.IsDashCharged)
+                            playerMovement.StartDash(new Vector3(-1, 0, -1));
+                        else
+                            playerMovement.StartMove(new Vector3(-1, 0, -1));
+
+                        hasSwiped = true;
+                    }
+
+                }
+            }
+        }
+    }
+
+
+    private bool SwipedLongEnough(Vector3 direction)
+    {
+        return (!(Math.Abs(direction.x) > horizontalSwipeDistance) &&
+            !(Math.Abs(direction.y) > verticalSwipeDistance));
+    }
+
+
+    private Vector3 HorizontalSwipe()
+    {
+        if (lastPosition.x > firstPosition.x)
+            return Vector3.right;
+
+        return Vector3.left;
+    }
+
+
+    private Vector3 VerticalSwipe()
+    {
+        if (lastPosition.y > firstPosition.y)
+            return Vector3.forward;
+
+        return Vector3.back;
+    }
+}
---
+++ Assets/Scripts/Pipelinetest/PlayerMovement.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int DashMultiplier = 2500;
+    public int MoveMultiplier = 1000;
+    public float CoolDownValue = 0.1f;
+    public float ChargeThreshold = 0.25f;
+    public float DashDuration = 0.1f;
+    public float MoveDuration = 0.2f;
+
+    //public float moveSpeed = 3f;
+    //public float range = 2f;
+
+    private Rigidbody rigidBody;
+    private Material material;
+    private Vector3 spawnPos;
+    private readonly int maxNegativeY = -5;
+    private bool isMoving;
+    private float colorValue = 1;
+
+    private Vector3 lastPosition;
+    private Grid grid;
+    private TrailRenderer trailRenderer;
+
+    public float Timer { get; set; }
+
+    public bool IsDashCharged { get; set; }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        material = GetComponent<Renderer>().material;
+        grid = FindObjectOfType<Grid>();
+        trailRenderer = GetComponent<TrailRenderer>();
+        trailRenderer.enabled = false;
+        spawnPos = rigidBody.position;
+
+        transform.position = grid.GetCellCenterWorld(new Vector3Int(-1, 0, -4));
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (rigidBody.position.y < maxNegativeY)
+        {
+            rigidBody.position = spawnPos;
+            rigidBody.velocity = Vector3.zero;
+        }
+    }
+
+    public void ChargeDash()
+    {
+        material.color = new Color(1, colorValue, colorValue, 1);
+        colorValue -= 0.05f;
+    }
+
+    public void StartDash(Vector3 dashDirection)
+    {
+        if (isMoving)
+            return;
+
+        trailRenderer.enabled = true;
+        isMoving = true;
+
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.AddForce(dashDirection * DashMultiplier);
+
+        ResetDash();
+
+        StartCoroutine(StartCoolDown(DashDuration));
+    }
+
+    public void ResetDash()
+    {
+        material.SetColor("_Color", Color.black);
+        colorValue = 1f;
+        IsDashCharged = false;
+    }
+
+    public void StartMove(Vector3 moveDirection)
+    {
+        if (isMoving)
+            return;
+
+        isMoving = true;
+
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.AddForce(moveDirection * MoveMultiplier);
+        StartCoroutine(StartCoolDown(MoveDuration));
+    }
+
+    IEnumerator StartCoolDown(float duration)
+    {
+        yield return  new WaitForSeconds(duration);
+        rigidBody.velocity = Vector3.zero;
+        trailRenderer.enabled = false;
+
+        yield return new WaitForSeconds(CoolDownValue);
+        isMoving = false;
+    }
+
+    //public void ContinuousMovement(Vector3 touchPosition)
+    //{
+    //    RaycastHit hit;
+    //    Ray ray = Camera.main.ScreenPointToRay(touchPosition);
+    //    if (Physics.Raycast(ray, out hit))
+    //    {
+    //        Vector3 targetPos = hit.point;
+    //        if (Vector3.Distance(targetPos, lastPosition) > range)
+    //        {
+    //            targetPos.y = transform.position.y;
+    //            transform.LookAt(targetPos);
+    //            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+    //        }
+
+    //        if (lastPosition == transform.position)
+    //        {
+    //            Timer += Time.deltaTime;
+    //            if (Timer >= ChargeThreshold)
+    //            {
+    //                ChargeDash();
+    //                IsDashCharged = true;
+    //            }
+    //        }
+    //        else
+    //        {
+    //            if (!IsDashCharged)
+    //            {
+    //                ResetDash();
+    //                Timer = 0;
+    //            }
+    //            else
+    //            {
+    //                Vector3 dashDirection = (transform.position - lastPosition).normalized;
+    //                StartDash(dashDirection);
+    //            }
+    //        }
+
+    //        lastPosition = transform.position;
+    //    }
+    //}
+}
