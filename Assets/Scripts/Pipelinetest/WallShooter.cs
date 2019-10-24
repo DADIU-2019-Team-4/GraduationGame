9bbe1344e4de1b49b8b17eb891d6fda4462db14a
magei00 <magnuseilersen@yahoo.dk>
2019-10-24 15:22:46 +0200

    Fixed custom editor preventing building

--- Assets/Scripts/Pipelinetest/WallShooter.cs
+++ Assets/Scripts/Pipelinetest/WallShooter.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 
 public class WallShooter : MonoBehaviour
 {
@@ -59,18 +58,3 @@ public class WallShooter : MonoBehaviour
 }
 
 
-[CustomEditor(typeof(WallShooter))]
-public class SomeScriptEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        DrawDefaultInspector();
-
-
-        WallShooter wallShooter = (WallShooter)target;
-        if (GUILayout.Button("spawn 'very cool' plane"))
-        {
-            wallShooter.SpawnPlane();
-        }
-    }
-}
