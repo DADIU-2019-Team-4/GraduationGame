0dc3b474fbfb8bdd27d48df0f2389a4f9306dc37
Roxanne van Dam <roxanne.vandam@outlook.com>
2019-11-01 16:04:55 +0100

    fixed teleportFuse

--- Assets/Scripts/Prototype/TeleportFuse.cs
+++ Assets/Scripts/Prototype/TeleportFuse.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class TeleportFuse : MonoBehaviour
 {
     public bool OnlyUsedOnce;
-    private GameObject startPt;
-    private GameObject endPt;
+    private Transform startPt;
+    private Transform endPt;
 
     private bool isUsed;
     private MovementController movementController;
@@ -18,8 +18,8 @@ public class TeleportFuse : MonoBehaviour
 
     private void Start()
     {
-        startPt = GameObject.Find("StartPoint");
-        endPt = GameObject.Find("EndPoint");
+        startPt = transform.Find("StartPoint");
+        endPt = transform.Find("EndPoint");
     }
 
     public void Follow(StartPoint.PointType pointType)
@@ -31,11 +31,11 @@ public class TeleportFuse : MonoBehaviour
 
         if (pointType == StartPoint.PointType.Start)
         {
-            StartCoroutine(FuseRoutine(endPt.transform.position));
+            StartCoroutine(FuseRoutine(endPt.position));
         }
         else if (pointType == StartPoint.PointType.End)
         {
-            StartCoroutine(FuseRoutine(startPt.transform.position));
+            StartCoroutine(FuseRoutine(startPt.position));
         }
     }
 
