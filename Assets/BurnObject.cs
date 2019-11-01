cf711e659e2dfbb8ee73c975fcf9e57d70a30d3b
Vladislav Zenkevic <vladislav.zenkevic@gmail.com>
2019-11-01 09:31:54 +0100

    init commit
    BurnObject script created

---
+++ Assets/BurnObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BurnObject : MonoBehaviour
+{
+    private bool _isburnable;
+    private bool _isBurning;
+    private bool _desroyAfterBurn;  
+
+    public void SetObjectOnFire()
+    {
+
+    }
+}
