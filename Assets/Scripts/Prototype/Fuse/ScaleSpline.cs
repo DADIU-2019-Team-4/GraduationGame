8808e0631cb8ffaf065b47b85d1056fa6d8e404b
Roxanne van Dam <roxanne.vandam@outlook.com>
2019-11-04 10:05:11 +0100

    made scaler for fuse

---
+++ Assets/Scripts/Prototype/Fuse/ScaleSpline.cs
@@ -0,0 +1,26 @@
+﻿using SplineMesh;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class ScaleSpline : MonoBehaviour
+{
+    private Spline spline;
+
+    private float startScale = 1;
+    public float Scale = 1;
+
+    // Update is called once per frame
+    void Update()
+    {
+        spline = GetComponent<Spline>();
+        if (spline == null)
+            return;
+
+        // Scales the spline
+        foreach (CubicBezierCurve curve in spline.GetCurves())
+        {
+            curve.n1.Scale = Vector2.one * (startScale + (Scale - startScale));
+            curve.n2.Scale = Vector2.one * (startScale + (Scale - startScale));
+        }
+    }
+}
--- Assets/Scripts/Prototype/Fuse/UpdateStartEndPoints.cs
+++ Assets/Scripts/Prototype/Fuse/UpdateStartEndPoints.cs
@@ -1,21 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using SplineMesh;
 
 [ExecuteInEditMode]
 public class UpdateStartEndPoints : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Spline spline;
 
     private Transform startPoint;
     private Transform endPoint;
-    private Spline spline;
-
-    private void Awake()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
@@ -23,8 +15,11 @@ public class UpdateStartEndPoints : MonoBehaviour
         startPoint = transform.Find("StartPoint");
         endPoint = transform.Find("EndPoint");
         spline = GetComponent<Spline>();
-        
+
+        if (spline == null)
+            return;
+
         startPoint.position = transform.position + spline.nodes[0].Position;
-        endPoint.position = transform.position + spline.nodes[spline.nodes.Count-1].Position;
+        endPoint.position = transform.position + spline.nodes[spline.nodes.Count - 1].Position;
     }
 }
