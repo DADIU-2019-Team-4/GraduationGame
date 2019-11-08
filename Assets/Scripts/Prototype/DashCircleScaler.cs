86013a6522c2801dea0a126609032bfa286d930d
Roxanne van Dam <roxanne.vandam@outlook.com>
2019-11-08 10:38:50 +0100

    made circle easy scalable

---
+++ Assets/Scripts/Prototype/DashCircleScaler.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class DashCircleScaler : MonoBehaviour
+{
+    private InputManager inputManager;
+    public RectTransform InnerCircle;
+    public RectTransform OuterCircle;
+
+    public float ScaleFactor = 2.019f;
+
+    private void Awake()
+    {
+        inputManager = FindObjectOfType<InputManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newMoveScale = inputManager.MoveThreshold * ScaleFactor;
+        float newDashScale = inputManager.DashThreshold * ScaleFactor;
+        InnerCircle.sizeDelta = new Vector2(newMoveScale, newMoveScale);
+        OuterCircle.sizeDelta = new Vector2(newDashScale, newDashScale);
+    }
+}
