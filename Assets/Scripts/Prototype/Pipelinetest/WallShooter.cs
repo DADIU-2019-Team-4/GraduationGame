782ff484088e8e11e5dbc790ef08f949d84be756
magei00 <magnuseilersen@yahoo.dk>
2019-11-18 10:43:24 +0100

    fixed wallshooter bug where a plane got stuck in the shooter

--- Assets/Scripts/Prototype/Pipelinetest/WallShooter.cs
+++ Assets/Scripts/Prototype/Pipelinetest/WallShooter.cs
@@ -8,7 +8,7 @@ public class WallShooter : MonoBehaviour
     //public float detectionDistance = 5;
     public GameObject plane;
     public float spawnCooldown;
-    private float currentSpawnCooldown=0;
+    private float currentSpawnCooldown;
     public float planeSpeed;
     public float planeBurnDuration;
     public float distancePlaneCanTravel;
@@ -17,31 +17,29 @@ public class WallShooter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        currentSpawnCooldown = spawnCooldown;
     }
 
     // Update is called once per frame
     private void Update()
     {
         currentSpawnCooldown -= Time.deltaTime;
-        SpawnPlane();
-
+        if (currentSpawnCooldown < 0)
+        {
+            SpawnPlane();
+        }
     }
 
     public void SpawnPlane()
     {
-        if (currentSpawnCooldown<0)
-        {
+        
             GameObject planeRef = GameObject.Instantiate(plane, transform.position, this.transform.rotation);
             planeRef.GetComponent<PaperPlane>().speed = planeSpeed;
             planeRef.GetComponent<PaperPlane>().burnDuration = planeBurnDuration;
             planeRef.GetComponent<PaperPlane>().distanceToTravel = distancePlaneCanTravel;
             currentSpawnCooldown = spawnCooldown;
-        }
-        else
-        {
-            //Debug.Log("Spawn on Cooldown: "+currentSpawnCooldown);
-        }
-       
+        
+        
     }
 }
 
