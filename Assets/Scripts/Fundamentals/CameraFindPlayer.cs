ab1fc8dea23dca43b9811edb5fcc1fa9980f6026
MichaeliusDenModige <s143049@student.dtu.dk>
2019-11-18 11:49:20 +0100

    Decentralized camera to local scenes.

---
+++ Assets/Scripts/Fundamentals/CameraFindPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraFindPlayer : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // Await MainPlayerScene
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (player == null) return;
+
+        // Set values.
+        var camera = GameObject.FindGameObjectWithTag("VirtualCamera").GetComponent<CinemachineVirtualCamera>();
+        camera.Follow = camera.LookAt = player.transform;
+
+        // Disable this script.
+        Destroy(this);
+    }
+}
