0f2412fb2616a3379d7bc0bb8af5da8a2b44de89
MichaeliusDenModige <s143049@student.dtu.dk>
2019-11-18 13:24:30 +0100

    UI: Framerate and current level

---
+++ Assets/Scripts/Fundamentals/Framerate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Framerate : IGameLoop
+{
+    private Text Text;
+    private float prevFPS;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text = GetComponent<Text>();
+        prevFPS = 45f;
+    }
+
+    // Update is called once per frame
+    public override void GameLoopUpdate()
+    {
+        prevFPS = (prevFPS + 1 / Time.deltaTime) / 2;
+        Text.text = "FPS: " + Mathf.Round(prevFPS);
+    }
+}
