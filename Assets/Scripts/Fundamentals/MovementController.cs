a1d34c9025887e20cfefd4bf73c6242497c9e4e8
magei00 <magnuseilersen@yahoo.dk>
2019-11-20 12:00:30 +0100

    Health decimal fix

--- Assets/Scripts/Fundamentals/MovementController.cs
+++ Assets/Scripts/Fundamentals/MovementController.cs
@@ -350,7 +350,7 @@ public class MovementController : MonoBehaviour
     /// </summary>
     private void UpdateFireAmountText()
     {
-        FireAmountText.text = currentFireAmount + "%";
+        FireAmountText.text = string.Format("{0:.#}", currentFireAmount) + "%";
     }
 
     /// <summary>
