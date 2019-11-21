6baf2a9a6b17820a93673ea62e8779a8cc9c4847
magei00 <magnuseilersen@yahoo.dk>
2019-11-21 12:12:17 +0100

    Updates to menu prefabs & continuebutton disabled first time playing

--- Assets/UINavigation.cs
+++ Assets/UINavigation.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UINavigation : MonoBehaviour
 {
@@ -10,16 +11,27 @@ public class UINavigation : MonoBehaviour
     public GameObject _mainMenu;
 
     public GameObject _pauseMenu;
+
+    public GameObject _continueButton;
     // Start is called before the first frame update
     void Awake()
     {
         InitPlayerPrefs();
+
+        if (PlayerPrefs.GetInt("FirstTimePlaying")==1)
+        {
+            _continueButton.GetComponent<Button>().interactable= false;
+        }
+
+
         //TODO if first time playing hide the continue button
 
 
         //_optionsMenu = GameObject.FindGameObjectWithTag("OptionsMenu");
         //_mainMenu = GameObject.FindGameObjectWithTag("MainMenu");
         //_pauseMenu = GameObject.FindGameObjectWithTag("PauseMenu");
+
+
 
         if (this.tag != "OptionsMenu")
         {
@@ -110,6 +122,11 @@ public class UINavigation : MonoBehaviour
         if (!PlayerPrefs.HasKey("Language"))
         {
             PlayerPrefs.SetString("Language","English");
+        }
+
+        if (!PlayerPrefs.HasKey("FirstTimePlaying"))
+        {
+            PlayerPrefs.SetInt("FirstTimePlaying", 1);
         }
     }
 }
