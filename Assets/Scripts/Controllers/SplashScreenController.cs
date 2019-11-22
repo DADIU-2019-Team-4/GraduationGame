928b9c2e5c31c8d4b2ac65b8d62e99474dc22683
magei00 <magnuseilersen@yahoo.dk>
2019-11-22 10:55:54 +0100

    Added Splash screen functionality from our previous game

---
+++ Assets/Scripts/Controllers/SplashScreenController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using DG.Tweening;
+
+public class SplashScreenController : MonoBehaviour
+{
+    [SerializeField]
+    private Image _companyName;
+    [SerializeField]
+    private Image _gameTitle;
+    [SerializeField]
+    private Image _headphones;
+    [SerializeField]
+    private float _fadeDuration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(StartIntro());
+    }
+
+    // Update is called once per frame
+
+    IEnumerator StartIntro()
+    {
+        AkSoundEngine.PostEvent("CompanyName_event", gameObject);
+        _companyName.DOFade(1f, _fadeDuration);
+        yield return new WaitForSeconds(_fadeDuration);
+        _companyName.DOFade(0f, _fadeDuration);
+        yield return new WaitForSeconds(_fadeDuration);
+
+        AkSoundEngine.PostEvent("GameTitle_event", gameObject);
+        _gameTitle.DOFade(1f, _fadeDuration);
+        yield return new WaitForSeconds(_fadeDuration);
+        _gameTitle.DOFade(0f, _fadeDuration);
+        yield return new WaitForSeconds(_fadeDuration);
+
+        AkSoundEngine.PostEvent("GameTitle_event", gameObject);
+        _headphones.DOFade(1f, _fadeDuration);
+        yield return new WaitForSeconds(_fadeDuration);
+        _headphones.DOFade(0f, _fadeDuration);
+        yield return new WaitForSeconds(_fadeDuration);
+
+        SceneManager.LoadScene("Intro Scene");
+    }
+}
