1d2dc93f4347a0d74c7256008cf72cf6416ee230
Mathias B <mathiasboldsen@gmail.com>
2019-11-22 11:19:05 +0100

    nice changes

---
+++ Assets/Scripts/ScriptsByDesigners/VCamChange.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VCamChange : MonoBehaviour
+{
+    public GameObject vcam;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(switchToCam());
+        }
+       // switchToThisVcam();
+    }
+
+
+   /* void OnTriggerExit(Collider other)
+    {
+        switchVcamOff();
+    } 
+    */
+
+    public void switchToThisVcam()
+    {
+        StartCoroutine(switchToCam()); 
+      
+    }
+
+   private IEnumerator switchToCam()
+    {
+        yield return new WaitForSeconds(2f);
+        vcam.gameObject.SetActive(true);
+        yield return new WaitForSeconds(5f);
+        vcam.gameObject.SetActive(false);
+    }
+}
