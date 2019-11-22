9221a69d28d6cddd4065143c952b2142e78e700f
Mathias B <mathiasboldsen@gmail.com>
2019-11-22 15:34:34 +0100

    There is now vibration... ya

--- Assets/Scripts/Fundamentals/InputManager.cs
+++ Assets/Scripts/Fundamentals/InputManager.cs
@@ -116,6 +116,7 @@ public class InputManager : IGameLoop
                 return;
             }
 
+            Vibration.Vibrate(5);
             // if dash is charged, change arrow to dash length
             StretchArrow(movementController.DashDistance);
             arrow.GetComponent<SpriteRenderer>().color = Color.red;
@@ -338,6 +339,10 @@ public class InputManager : IGameLoop
 
         if (movementController.IsDashCharged)
         {
+
+            //insert vibration for release dash
+            Vibration.Vibrate(300);
+
             movementController.Dash(directionVector.normalized);
             ResetDash();
         }
@@ -356,5 +361,64 @@ public class InputManager : IGameLoop
         movementController.ResetDash();
         dashTimer = 0;
         movementController.DashHoldPercentage.Value = dashTimer;
+    }
+}
+
+public static class Vibration
+{
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+    public static AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+    public static AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+    public static AndroidJavaObject vibrator = currentActivity.Call<AndroidJavaObject>("getSystemService", "vibrator");
+#else
+    public static AndroidJavaClass unityPlayer;
+    public static AndroidJavaObject currentActivity;
+    public static AndroidJavaObject vibrator;
+#endif
+
+    public static void Vibrate()
+    {
+        if (isAndroid())
+            vibrator.Call("vibrate");
+        else
+            Handheld.Vibrate();
+    }
+
+
+    public static void Vibrate(long milliseconds)
+    {
+        if (isAndroid())
+            vibrator.Call("vibrate", milliseconds);
+        else
+            Handheld.Vibrate();
+    }
+
+    public static void Vibrate(long[] pattern, int repeat)
+    {
+        if (isAndroid())
+            vibrator.Call("vibrate", pattern, repeat);
+        else
+            Handheld.Vibrate();
+    }
+
+    public static bool HasVibrator()
+    {
+        return isAndroid();
+    }
+
+    public static void Cancel()
+    {
+        if (isAndroid())
+            vibrator.Call("cancel");
+    }
+
+    private static bool isAndroid()
+    {
+#if UNITY_ANDROID && !UNITY_EDITOR
+	return true;
+#else
+        return false;
+#endif
     }
 }
