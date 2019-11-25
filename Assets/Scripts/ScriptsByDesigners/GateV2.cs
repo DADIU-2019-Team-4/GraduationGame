4ac841c76ed7c68262bfe6cef7f09b4618e3684d
Mathias B <mathiasboldsen@gmail.com>
2019-11-25 22:33:53 +0100

    Made more on levels, Put in some prefabs that Victor bought. Made a new script for the gates and applied them in room1Level1

---
+++ Assets/Scripts/ScriptsByDesigners/GateV2.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateV2 : MonoBehaviour
+{
+    public GameObject Door;
+    public float DelaySeconds = 0f;
+
+    private List<AudioEvent> audioEvents;
+    private bool _triggerUnlock = false;
+
+    //gate bool
+    private bool triggerGate = false; 
+
+    private void Awake()
+    {
+        audioEvents = new List<AudioEvent>(GetComponents<AudioEvent>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (triggerGate && _triggerUnlock == false)
+        {
+            Debug.Log("Happened"); 
+            _triggerUnlock = true;
+            StartCoroutine(Unlock());
+        }
+    }
+
+    IEnumerator Unlock()
+    {
+        yield return new WaitForSeconds(DelaySeconds);
+        AudioEvent.SendAudioEvent(AudioEvent.AudioEventType.GateUnlocked, audioEvents, gameObject);
+        Door.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("I'm alive!!!"); 
+            triggerGate = true;
+        }
+    }
+}
