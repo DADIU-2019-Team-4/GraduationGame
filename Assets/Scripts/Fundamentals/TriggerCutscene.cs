5c2ea2d89b14e414ff4da5b72886163a31da03c8
MichaeliusDenModige <s143049@student.dtu.dk>
2019-11-26 11:09:04 +0100

    Added Trigger Custscene

---
+++ Assets/Scripts/Fundamentals/TriggerCutscene.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Playables;
+
+public class TriggerCutscene : MonoBehaviour
+{
+    private PlayableDirector _timeline;
+    public UnityEvent OnTrigger;
+
+    public bool OnlyOnce = true;
+
+    private void Awake()
+    {
+        _timeline = GetComponent<PlayableDirector>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            //SetBindings();
+            OnTrigger.Invoke();
+            _timeline.Play();
+
+            if (OnlyOnce)
+                Destroy(this);
+        }
+
+
+    }
+
+    private void SetBindings()
+    {
+        var timelineAsset = _timeline.playableAsset;
+        //for (int i = 0; i < tracklist)
+    }
+
+}
