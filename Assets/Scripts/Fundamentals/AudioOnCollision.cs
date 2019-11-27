4aad3ef97a463153cafd4fc2fa3544aae1f2b66d
pvejen <vejenpatrick@gmail.com>
2019-11-27 13:30:45 +0100

    Music

--- Assets/Scripts/Fundamentals/AudioOnCollision.cs
+++ Assets/Scripts/Fundamentals/AudioOnCollision.cs
@@ -10,9 +10,10 @@ public class AudioOnCollision : MonoBehaviour
         audioEvents = new List<AudioEvent>(GetComponents<AudioEvent>());
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player")
             AudioEvent.SendAudioEvent(AudioEvent.AudioEventType.OnCollision, audioEvents, gameObject);
+        Debug.Log("SoundCollision");
     }
 }
