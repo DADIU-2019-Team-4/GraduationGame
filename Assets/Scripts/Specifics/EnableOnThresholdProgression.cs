f8524b524e178a67e9c43cb5b547ccdb6c4f3b3e
MichaeliusDenModige <s143049@student.dtu.dk>
2019-11-29 10:40:11 +0100

    GameObject dis-/enabling by story progression

---
+++ Assets/Scripts/Specifics/EnableOnThresholdProgression.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableOnThresholdProgression : MonoBehaviour
+{
+    public StoryProgression.EStoryProgression Stage;
+    public StoryProgression Progression;
+
+    [TextArea]
+    public string Message = "This GameObject is enabled on start if the player has at least reached the given stage.";
+    // Start is called before the first frame update
+    void Start()
+    {
+        if ((int)Progression.Value < (int)Stage)
+            gameObject.SetActive(false);
+    }
+
+}
