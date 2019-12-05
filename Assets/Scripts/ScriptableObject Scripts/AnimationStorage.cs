6e5aa942c6305c4bb4ef165c139e1afaf4cd2411
Vladislav <vladislav.zenkevic@gmail.com>
2019-12-05 11:54:02 +0100

    Removed QA files + added storage system to splash screen

---
+++ Assets/Scripts/ScriptableObject Scripts/AnimationStorage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AnimationStorage", menuName = "ScriptableObjects/AnimationStorage")]
+public class AnimationStorage : ScriptableObject
+{
+    public TextAsset[] MoMaCSV;
+    public GameObject[] Models;
+
+}
---
+++ Assets/StoreOBB.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class StoreOBB : MonoBehaviour
+{
+    public ScriptableObject Files;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+}
